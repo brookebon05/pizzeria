40a65e01c2ac50927feb744532a2b19a08e16b8a
Brooke Bonadies <Brooke_Bonadies2@baylor.edu>
2021-03-03 14:15:39 -0600

    3.3.2021

--- NumPy/MyArrays.py
+++ NumPy/MyArrays.py
@@ -1,3 +1,16 @@
 import numpy as np
 
 # cntrl shift P, start REPL
+integers = np.array([1, 2, 3])
+print(type(integers))
+# prints
+
+# create 1D array producing list of ints 2-20
+
+new_ints = np.array([i for i in range(2,21,2])
+print(new_ints)
+
+#2d array
+ints = np.array([1,2,3],[4,5,6]]) #two rows of 3 columns
+ints2 = np.array([0.0, 0.1, 0.2, 0.3, 0.4]) #1D row of floats 0 = 0. 
+
