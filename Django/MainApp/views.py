83e471314c8321febd96206b80e579a6d9b42ddf
Brooke Bonadies <Brooke_Bonadies2@baylor.edu>
2021-04-19 14:19:49 -0500

    4.19.2021

--- Django/MainApp/views.py
+++ Django/MainApp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Topic
+from .forms import TopicForm
 
 # Create your views here.
 
@@ -6,3 +8,30 @@ from django.shortcuts import render
 def index(request):
     # The home page for LEarning Log
     return render(request, "MainApp/index.html")
+
+
+def topics(request):
+    topics = Topic.objects.order_by("date_added")
+    context = {"topics": topics}
+    return render(request, "MainApp/topics.html", context)
+
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by("-date_added")
+    context = {"topic": topic, "entries": entries}
+    return render(request, "MainApp/topic.html", context)
+
+
+# GET AND POST ARE TWO WAYS USER CAN INTERACT
+def new_topic(request):
+    if request.method != "POST":  # is a get request
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request)
+        if form.is_valid():
+            form.save()
+
+            return redirect("MainApp:topics")
+    context = {"form": form}
+    return render(request, "MainApp/new_topic.html", context)
--- Django/learning_log/urls.py
+++ Django/learning_log/urls.py
@@ -16,4 +16,4 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("mainapp.urls"))]
+urlpatterns = [path("admin/", admin.site.urls), path("", include("MainApp.urls"))]
